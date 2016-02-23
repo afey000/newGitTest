15e5ba2cb8102eec9faef94abe5a3078d715c3a3
Afrah <administrator@Administrator-Mackook.local>
2016-02-23 19:28:06 +0500

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+confirm("its working");
+$(document).ready(function(){
+	$('div').mouseenter(function(){
+    $('.red , .pink').fadeIn('slow',0);
+	});
+	$('div').mouseleve(function(){
+		$('.red , .pink')fadeOut('slow',20);
+	});
+});
